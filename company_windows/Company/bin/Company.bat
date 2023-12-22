c80b06c21129d7ff67c392c4120e53d2445a2866
Hristian Sarmov <137688109+HristianSarmov@users.noreply.github.com>
2023-12-22 14:29:59 +0200

    Delete company_windows directory

--- company_windows/Company/bin/Company.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DIR="%~dp0"
-set JAVA_EXEC="%DIR:"=%\java"
-
-
-
-pushd %DIR% & %JAVA_EXEC% %CDS_JVM_OPTS% -cp sqlite-jdbc-3.34.0.jar -p "%~dp0/../app" -m Company/org.openjfx.Main  %* 2> nul & popd
--- company_windows/README.md
+++
@@ -1,3 +0,0 @@
-## How to use: 
-
-To use this application locate the Company.vbs file inside the bin folder and execute it.
